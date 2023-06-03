fbfb77ab30ca65c76dfe08b9c7b60b9ef3617193
Shannon Rice <srice9036@gmail.com>
2023-06-03 21:46:19 +0100

    [#1] {t:300} {shared_mathematics:main} Added vector classes and tests as well as initial file for matricies

---
+++ geometry/vector.py
@@ -0,0 +1,135 @@
+"""
+This module provides classes for vector operations in both 2D and 3D space.
+
+Classes:
+    vector_base: Abstract base class representing a vector.
+    vector: A class representing a vector in 2D space. Inherits from vector_base.
+    vector_3D: A class representing a vector in 3D space. Inherits from vector_base.
+
+"""
+from __future__ import annotations
+from abc import ABC
+
+
+class vector_base(ABC):
+    """Abstract base class representing a vector."""
+
+    def transpose(self) -> None:
+        """Transposes the vector by swapping its x and y coordinates."""
+        temp_x = self._dimensions[0]
+        temp_y = self._dimensions[1]
+        self.dimensions = (temp_y, temp_x)
+
+    @property
+    def dimensions(self) -> tuple:
+        """Getter property that returns the dimensions of the vector."""
+        return self._dimensions
+
+    @dimensions.setter
+    def dimensions(self, new_dimensions: tuple) -> None:
+        """Setter property that sets the dimensions of the vector."""
+        self._dimensions = new_dimensions
+
+
+class vector(vector_base):
+    """A class representing a vector. Inherits from vector_base which provides basic vector operations"""
+
+    def __init__(self, x: float, y: float) -> None:
+        """Initializes a vector instance with given x and y coordinates."""
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.dimensions = (1, 2)
+
+    def __add__(self, addition_vector: vector) -> vector:
+        """Adds another vector to the current vector and returns the result."""
+        new_x = addition_vector.x + self.x
+        new_y = addition_vector.y + self.y
+        return vector(new_x, new_y)
+
+    def __iadd__(self, addition_vector: vector) -> vector:
+        """Adds another vector to the current vector in-place."""
+        self.x += addition_vector.x
+        self.y += addition_vector.y
+        return self
+
+    def __mul__(self, multiplication_vector: vector) -> vector:
+        """Multiplies the current vector by another vector and returns the result."""
+        new_x = multiplication_vector.x * self.x
+        new_y = multiplication_vector.y * self.y
+        return vector(new_x, new_y)
+
+    def __imul__(self, multiplication_vector: vector) -> vector:
+        """Multiplies the current vector by another vector in-place."""
+        self.x *= multiplication_vector.x
+        self.y *= multiplication_vector.y
+        return self
+
+    def __sub__(self, subtraction_vector: vector) -> vector:
+        """Subtracts another vector from the current vector and returns the result."""
+        new_x = self.x - subtraction_vector.x
+        new_y = self.y - subtraction_vector.y
+        return vector(new_x, new_y)
+
+    def __isub__(self, subtraction_vector: vector) -> vector:
+        """Subtracts another vector from the current vector in-place."""
+        self.x -= subtraction_vector.x
+        self.y -= subtraction_vector.y
+        return self
+
+
+class vector_3D(vector_base):
+    """A class representing a 3d vector with an x, y and z axis. Inherits from vector_base which provides basic vector operations"""
+
+    def __init__(self, x: float, y: float, z: float) -> None:
+        """Initializes a vector instance with given x, y and z coordinates."""
+
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.z = z
+        self.dimensions = (1, 3)
+
+    def __add__(self, addition_vector: vector_3D) -> vector:
+        """Adds another vector to the current vector and returns the result."""
+
+        new_x = addition_vector.x + self.x
+        new_y = addition_vector.y + self.y
+        new_z = addition_vector.z + self.z
+        return vector_3D(new_x, new_y, new_z)
+
+    def __iadd__(self, addition_vector: vector_3D) -> vector:
+        """Adds another vector to the current vector in-place."""
+
+        self.x += addition_vector.x
+        self.y += addition_vector.y
+        self.z += addition_vector.z
+        return self
+
+    def __mul__(self, multiplication_vector: vector_3D) -> vector:
+        """Multiplies the current vector by another vector and returns the result."""
+        new_x = self.x * multiplication_vector.x
+        new_y = self.y * multiplication_vector.y
+        new_z = self.z * multiplication_vector.z
+        return vector_3D(new_x, new_y, new_z)
+
+    def __imul__(self, multiplication_vector: vector_3D) -> vector:
+        """Multiplies the current vector by another vector in-place."""
+        self.x *= multiplication_vector.x
+        self.y *= multiplication_vector.y
+        self.z *= multiplication_vector.z
+        return self
+
+    def __sub__(self, subtraction_vector: vector_3D) -> vector:
+        """Subtracts another vector from the current vector and returns the result."""
+        new_x = self.x - subtraction_vector.x
+        new_y = self.y - subtraction_vector.y
+        new_z = self.z - subtraction_vector.z
+        return vector_3D(new_x, new_y, new_z)
+
+    def __isub__(self, subtraction_vector: vector_3D) -> vector:
+        """Subtracts another vector from the current vector in-place."""
+        self.x -= subtraction_vector.x
+        self.y -= subtraction_vector.y
+        self.z -= subtraction_vector.z
+        return self
---
+++ tests/test_vector.py
@@ -0,0 +1,131 @@
+"""Module to test all functionality in vector module"""
+
+from geometry.vector import vector, vector_3D
+
+
+def test_vector_addition():
+    """Test vector addition."""
+    v1 = vector(2, 3)
+    v2 = vector(4, 5)
+    result = v1 + v2
+    assert result.x == 6
+    assert result.y == 8
+
+
+def test_vector_in_place_addition():
+    """Test in-place vector addition."""
+    v1 = vector(2, 3)
+    v2 = vector(4, 5)
+    v1 += v2
+    assert v1.x == 6
+    assert v1.y == 8
+
+
+def test_vector_multiplication():
+    """Test vector multiplication."""
+    v1 = vector(2, 3)
+    v2 = vector(4, 5)
+    result = v1 * v2
+    assert result.x == 8
+    assert result.y == 15
+
+
+def test_vector_in_place_multiplication():
+    """Test in-place vector multiplication."""
+    v1 = vector(2, 3)
+    v2 = vector(4, 5)
+    v1 *= v2
+    assert v1.x == 8
+    assert v1.y == 15
+
+
+def test_vector_subtraction():
+    """Test vector subtraction."""
+    v1 = vector(4, 5)
+    v2 = vector(2, 3)
+    result = v1 - v2
+    assert result.x == 2
+    assert result.y == 2
+
+
+def test_vector_in_place_subtraction():
+    """Test in-place vector subtraction."""
+    v1 = vector(4, 5)
+    v2 = vector(2, 3)
+    v1 -= v2
+    assert v1.x == 2
+    assert v1.y == 2
+
+
+def test_vector_transpose():
+    """Test vector transposition."""
+    v1 = vector(4, 5)
+    v1.transpose()
+    assert v1.dimensions == (2, 1)
+
+
+def test_vector_3D_addition():
+    """Test 3D vector addition."""
+    v1 = vector_3D(2, 3, 4)
+    v2 = vector_3D(4, 5, 6)
+    result = v1 + v2
+    assert result.x == 6
+    assert result.y == 8
+    assert result.z == 10
+
+
+def test_vector_3D_in_place_addition():
+    """Test in-place 3D vector addition."""
+    v1 = vector_3D(2, 3, 4)
+    v2 = vector_3D(4, 5, 6)
+    v1 += v2
+    assert v1.x == 6
+    assert v1.y == 8
+    assert v1.z == 10
+
+
+def test_vector_3D_multiplication():
+    """Test 3D vector multiplication."""
+    v1 = vector_3D(2, 3, 4)
+    v2 = vector_3D(4, 5, 6)
+    result = v1 * v2
+    assert result.x == 8
+    assert result.y == 15
+    assert result.z == 24
+
+
+def test_vector_3D_in_place_multiplication():
+    """Test in-place 3D vector multiplication."""
+    v1 = vector_3D(2, 3, 4)
+    v2 = vector_3D(4, 5, 6)
+    v1 *= v2
+    assert v1.x == 8
+    assert v1.y == 15
+    assert v1.z == 24
+
+
+def test_vector_3D_subtraction():
+    """Test 3D vector subtraction."""
+    v1 = vector_3D(4, 5, 6)
+    v2 = vector_3D(2, 3, 4)
+    result = v1 - v2
+    assert result.x == 2
+    assert result.y == 2
+    assert result.z == 2
+
+
+def test_vector_3D_in_place_subtraction():
+    """Test in-place 3D vector subtraction."""
+    v1 = vector_3D(4, 5, 6)
+    v2 = vector_3D(2, 3, 4)
+    v1 -= v2
+    assert v1.x == 2
+    assert v1.y == 2
+    assert v1.z == 2
+
+
+def test_vector_3D_transpose():
+    """Test 3D vector transposition."""
+    v1 = vector_3D(4, 5, 6)
+    v1.transpose()
+    assert v1.dimensions == (3, 1)
